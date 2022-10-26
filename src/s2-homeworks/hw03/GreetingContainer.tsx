5dcbbea49b74fb5b2d75a5cfc8627d1b24870d3a
HuziMuzi <laif17860@mail.ru>
2022-10-26 12:31:43 +0300

    done h3

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -60,7 +60,7 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
         pureOnEnter(e, addUser)
     }
 
-    const totalUsers = users.length // need to fix
+    const totalUsers = users.length
     const lastUserName = users.length ? users[users.length-1].name : 'Пока ни кого не было' // need to fix
 
     return (
